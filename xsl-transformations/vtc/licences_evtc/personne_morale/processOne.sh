8e8cfc3940e28bd563122aa3886dffdb7f087eb7
Arie Benichou <arie.benichou@gmail.com>
2015-06-26 02:28:27 +0200

    updated scripts

--- xsl-transformations/vtc/licences_evtc/personne_morale/processOne.sh
+++ xsl-transformations/vtc/licences_evtc/personne_morale/processOne.sh
@@ -1,4 +1,12 @@
-#!/bin/sh 
+#!/bin/sh
 input=${1:-input.html}
 output=${2:-output.xml}
+
+echo -n "$input: "
 java net.sf.saxon.Transform -s:$input -xsl:../transformation.xsl -o:$output
+if [ $? -eq 0 ]
+then
+    echo ok >&2
+else
+    echo failed >&2
+fi
--- xsl-transformations/vtc/licences_evtc/personne_physique/processAll.sh
+++ xsl-transformations/vtc/licences_evtc/personne_physique/processAll.sh
@@ -5,5 +5,5 @@ output=${2:-/media/webdav/vtc/licences/personne-physique/xml}
 for file in $input/*
 do
 out=$output/$(basename $file .html).xml
-./processOne.sh $file $out && cat $out
+./processOne.sh $file $out
 done
