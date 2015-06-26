ed6e30274cd13e835aaa7527e44cd27511d465e7
Arie Benichou <arie.benichou@gmail.com>
2015-06-26 18:42:23 +0200

    minor changes

--- xsl-transformations/vtc/licences_evtc/personne_morale/processAll.sh
+++ xsl-transformations/vtc/licences_evtc/personne_morale/processAll.sh
@@ -3,7 +3,13 @@ input=${1:-/media/webdav/vtc/licences/personne-morale/html}
 output=${2:-/media/webdav/vtc/licences/personne-morale/xml}
 
 for file in $input/*
-do
-out=$output/$(basename $file .html).xml
-./processOne.sh $file $out
+  do
+    out=$output/$(basename $file .html).xml
+    if [ -f $out ]
+      then
+      echo "File $out already exists"
+    else
+      echo "File $out does not exist yet"
+    fi
+    ./processOne.sh $file $out
 done
--- xsl-transformations/vtc/licences_evtc/personne_morale/rdf-processAll.sh
+++ xsl-transformations/vtc/licences_evtc/personne_morale/rdf-processAll.sh
@@ -6,6 +6,12 @@ ordinal=1;
 for file in $input/*
   do
     out=$output/$(basename $file .xml).rdf;
+    if [ -f $out ]
+      then
+      echo "File $out already exists"
+    else
+      echo "File $out does not exist yet"
+    fi
     ./rdf-processOne.sh $file $out $ordinal;
     ordinal=$((ordinal+1))
   done
